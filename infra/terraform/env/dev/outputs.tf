9bfb9ae9fd6ebe9a12bb431392aafcce0c4cb687
JKleinne <jkq@jkleinne.com>
2024-09-12 14:33:04 -0400

    fix: fixed broken outputs.tf association between module and env

---
+++ infra/terraform/env/dev/outputs.tf
@@ -0,0 +1,11 @@
+output container_id {
+  value = module.postgres_dev.container_id
+}
+
+output container_name {
+  value = module.postgres_dev.container_name 
+}
+
+output container_network {
+  value = module.postgres_dev.container_network
+}
