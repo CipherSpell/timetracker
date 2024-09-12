9ef2070ff997ab8c0922b1381e4af39cc2c6c841
JKleinne <jkq@jkleinne.com>
2024-09-12 18:20:58 -0400

    feaet(iac): add extra output variables to be used by ansible

--- infra/terraform/env/dev/outputs.tf
+++ infra/terraform/env/dev/outputs.tf
@@ -9,3 +9,20 @@ output container_name {
 output container_network {
   value = module.postgres_dev.container_network
 }
+
+# Outputs needed for ansible
+output db_name {
+  value = module.postgres_dev.db_name 
+}
+
+output db_login_user {
+  value = module.postgres_dev.db_login_user 
+}
+
+output db_login_password {
+  value = module.postgres_dev.db_login_password
+}
+
+output db_port {
+  value = module.postgres_dev.db_port
+}
--- infra/terraform/modules/postgres_db/outputs.tf
+++ infra/terraform/modules/postgres_db/outputs.tf
@@ -9,3 +9,20 @@ output container_name {
 output container_network {
   value = docker_container.postgres.network_data
 }
+
+# Outputs needed for ansible
+output db_name {
+  value = var.postgres_db 
+}
+
+output db_login_user {
+  value = var.postgres_user 
+}
+
+output db_login_password {
+  value = var.postgres_password
+}
+
+output db_port {
+  value = var.external_port
+}
