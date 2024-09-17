348530c6b37ebb8ca54b58c619ef603648cf5e2d
Darrell-David <darrellmadanay@gmail.com>
2024-09-17 16:56:15 -0400

    feat: temporarily commented the <form> tag from the login page

--- landing/components/LoginForm.tsx
+++ landing/components/LoginForm.tsx
@@ -20,52 +20,52 @@ export const LoginForm: React.FC = () => {
         <div className="bg-black grid place-items-center h-screen">
             <div className="rounded-lg bg-stone-900 px-10 py-10 w-96">
                 <h2 className="text-white text-center text-2xl mb-12">
-                    Timekeeper 
+                    Timekeeper
                 </h2>
-                <form>
-                    <div>
-                        <label className="text-white mb-2 block text-sm leading-none">
-                            Email address
-                        </label>
-                        <Input
-                            id="email"
-                            name="email"
-                            placeholder="john.doe@example.com"
-                            onChange={(e) =>
-                                setFormInfo({
-                                    ...formInfo,
-                                    email: e.target.value,
-                                })
-                            }
-                        />
-                    </div>
-                    <div>
-                        <label className="text-white mb-2 block text-sm leading-none">
-                            Password
-                        </label>
-                        <Input
-                            id="password"
-                            name="password"
-                            placeholder="************"
-                            type="password"
-                            onChange={(e) =>
-                                setFormInfo({
-                                    ...formInfo,
-                                    password: e.target.value,
-                                })
-                            }
-                        />
-                    </div>
-                    <div>
-                        <button
-                            className="bg-white rounded-md w-full text-sm py-1"
-                            // formAction={login}
-                        >
-                            Sign in
-                        </button>
-                        <hr className="border-subtle my-8" />
-                    </div>
-                </form>
+                {/* <form> */}
+                <div>
+                    <label className="text-white mb-2 block text-sm leading-none">
+                        Email address
+                    </label>
+                    <Input
+                        id="email"
+                        name="email"
+                        placeholder="john.doe@example.com"
+                        onChange={(e) =>
+                            setFormInfo({
+                                ...formInfo,
+                                email: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div>
+                    <label className="text-white mb-2 block text-sm leading-none">
+                        Password
+                    </label>
+                    <Input
+                        id="password"
+                        name="password"
+                        placeholder="************"
+                        type="password"
+                        onChange={(e) =>
+                            setFormInfo({
+                                ...formInfo,
+                                password: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div>
+                    <button
+                        className="bg-white rounded-md w-full text-sm py-1"
+                        // formAction={login}
+                    >
+                        Sign in
+                    </button>
+                    <hr className="border-subtle my-8" />
+                </div>
+                {/* </form> */}
                 <Link href="/signup">
                     <button className="bg-white rounded-md w-full text-sm py-1">
                         Sign up
