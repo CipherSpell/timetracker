4bd2ad08788746fb2b3b98dba17c7412caa4be37
barangana <baranganadriel@gmail.com>
2024-09-19 21:25:02 -0400

    chore(landing): temporary fix to satisfy ci build

--- landing/src/components/Timetracker.tsx
+++ landing/src/components/Timetracker.tsx
@@ -2,13 +2,13 @@
 
 import { useState } from 'react'
 
-interface TimerProps {
-  minutes?: number
-}
+// interface TimerProps {
+//   minutes?: number
+// }
 
-const Timetracker: React.FC<TimerProps> = ({ minutes }) => {
+const Timetracker: React.FC<> = () => {
   const [started, setStarted] = useState<boolean>(false)
-  const [time, setTime] = useState<string>('00:00')
+  //   const [time, setTime] = useState<string>('00:00')
 
   const handleClick = () => {
     setStarted(!started)
@@ -16,7 +16,8 @@ const Timetracker: React.FC<TimerProps> = ({ minutes }) => {
 
   return (
     <div className='flex flex-col min-h-screen justify-center items-center'>
-      <div className='text-9xl mb-8'>{time}</div>
+      {/* <div className='text-9xl mb-8'>{time}</div> */}
+      <div className='text-9xl mb-8'>00:00</div>
       <div className='w-20'>
         <button
           className={
