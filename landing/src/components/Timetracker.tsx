2b0f837c9877ab28933f52067e88fa7a7fc514dc
barangana <baranganadriel@gmail.com>
2024-09-22 18:58:49 -0400

    feat(landing): timetracker component functionality done

--- landing/src/components/Timetracker.tsx
+++ landing/src/components/Timetracker.tsx
@@ -1,23 +1,44 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-// interface TimerProps {
-//   minutes?: number
-// }
+interface TimerProps {
+  minutes?: number
+}
 
-const Timetracker: React.FC = () => {
+const Timetracker: React.FC<TimerProps> = () => {
   const [started, setStarted] = useState<boolean>(false)
-  //   const [time, setTime] = useState<string>('00:00')
+  const [time, setTime] = useState<number>(30 * 60)
 
   const handleClick = () => {
     setStarted(!started)
   }
 
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined
+
+    if (started && time > 0) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+      }, 1000)
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId) // Clean up interval on pause/unmount
+    }
+  }, [started, time])
+
+  const formatTime = (seconds: number): string => {
+    const minutes = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    return `${minutes.toString().padStart(2, '0')}:${secs
+      .toString()
+      .padStart(2, '0')}`
+  }
+
   return (
     <div className='flex flex-col min-h-screen justify-center items-center'>
-      {/* <div className='text-9xl mb-8'>{time}</div> */}
-      <div className='text-9xl mb-8'>00:00</div>
+      <div className='text-9xl mb-8'>{formatTime(time)}</div>
       <div className='w-20'>
         <button
           className={
