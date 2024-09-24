22011eab8954d331dec7fb3be11adfa3571afbf9
barangana <baranganadriel@gmail.com>
2024-09-23 22:49:16 -0400

    fix(backend): fix erroneous logging on the backend when starting

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const winston = require('winston');
-const morgan = require('morgan');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const winston = require('winston')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
 const logger = winston.createLogger({
   level: 'info',
@@ -14,39 +14,38 @@ const logger = winston.createLogger({
     winston.format.simple()
   ),
   transports: [new winston.transports.Console()],
-});
+})
 
 logger.log({
   level: 'info',
-  message: `Starting server on port: ${process.env.PORT || 3000}`
-});
+  message: `Starting server on port: ${process.env.PORT || 8080}`,
+})
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-require('./routes/index')(app);
+require('./routes/index')(app)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res
-    .status(err.status || 500);
-});
+  res.status(err.status || 500)
+})
 
 logger.log({
   level: 'info',
-  message: 'Server started...'
-});
+  message: 'Server started...',
+})
 
-module.exports = app;
+module.exports = app
