0b3b8f8b02f430bfa89be4a9f2437b2ad2b06fad
Darrell-David <darrellmadanay@gmail.com>
2024-09-27 00:08:09 -0400

    feat(website): added validation to login inputs

--- landing/src/components/auth/login/LoginActions.ts
+++ landing/src/components/auth/login/LoginActions.ts
@@ -2,16 +2,9 @@
 
 import { userSchema } from '@/src/config/zod'
 
-export type State = {
-  errors: {
-    email?: string[]
-    password?: string[]
-  }
-}
-
 //TODO: modify "any" type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createUser = async (prevState: any, formData: FormData) => {
+export const getUser = async (prevState: any, formData: FormData) => {
   const validatedFields = userSchema.safeParse({
     email: formData.get('email'),
     password: formData.get('password'),
@@ -23,5 +16,5 @@ export const createUser = async (prevState: any, formData: FormData) => {
     }
   }
 
-  // TODO: make request to backend 
+  // TODO: make request to backend
 }
--- landing/src/components/auth/login/LoginForm.tsx
+++ landing/src/components/auth/login/LoginForm.tsx
@@ -6,13 +6,24 @@
 import React, { useState } from 'react'
 import { Input } from '../../Input'
 import Link from 'next/link'
+import { State } from '../register/RegisterActions'
+import { useFormState } from 'react-dom'
+import { getUser } from './LoginActions'
 
 interface FormData {
   email: string
   password: string
 }
 
+const initialState: State = {
+  errors: {
+    email: [''],
+    password: [''],
+  },
+}
+
 export const LoginForm: React.FC = () => {
+  const [state, formAction] = useFormState(getUser, initialState)
   const [formInfo, setFormInfo] = useState<FormData>({
     email: '',
     password: '',
@@ -22,45 +33,64 @@ export const LoginForm: React.FC = () => {
     <div className='bg-black grid place-items-center h-screen'>
       <div className='rounded-lg bg-stone-900 px-10 py-10 w-96'>
         <h2 className='text-white text-center text-2xl mb-12'>Timetracker</h2>
-        <div>
-          <label className='text-white mb-2 block text-sm leading-none'>
-            Email address
-          </label>
-          <Input
-            id='email'
-            name='email'
-            placeholder='john.doe@example.com'
-            onChange={(e) =>
-              setFormInfo({
-                ...formInfo,
-                email: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <label className='text-white mb-2 block text-sm leading-none'>
-            Password
-          </label>
-          <Input
-            id='password'
-            name='password'
-            placeholder='************'
-            type='password'
-            onChange={(e) =>
-              setFormInfo({
-                ...formInfo,
-                password: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <button className='bg-white rounded-md w-full text-sm py-1'>
-            Sign In
-          </button>
-          <hr className='border-subtle my-8' />
-        </div>
+        <form action={formAction}>
+          <div>
+            <label className='text-white mb-2 block text-sm leading-none'>
+              Email address
+            </label>
+            <Input
+              id='email'
+              name='email'
+              placeholder='john.doe@example.com'
+              type='email'
+              required
+              onChange={(e) =>
+                setFormInfo({
+                  ...formInfo,
+                  email: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div>
+            <label className='text-white mb-2 block text-sm leading-none'>
+              Password
+            </label>
+            <Input
+              id='password'
+              name='password'
+              placeholder='************'
+              type='password'
+              required
+              onChange={(e) =>
+                setFormInfo({
+                  ...formInfo,
+                  password: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div>
+            <button
+              className='bg-white rounded-md w-full text-sm py-1'
+              type='submit'
+            >
+              Sign In
+            </button>
+            <div
+              className='text-red-500'
+              id='error'
+              aria-label='polite'
+              aria-atomic='true'
+            >
+              {state?.errors?.password &&
+                state?.errors.password?.map((error: string) => {
+                  return <p key={'wee'}>{error}</p>
+                })}
+            </div>
+            <hr className='border-subtle my-8' />
+          </div>
+        </form>
         <Link href='/register'>
           <button className='bg-white rounded-md w-full text-sm py-1'>
             Sign Up
