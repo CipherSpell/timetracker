23a4734b58ce887d3e3ed8ba141579fb4a2f2dd3
Darrell-David <darrellmadanay@gmail.com>
2024-10-04 12:25:43 -0400

    feat (website): Added 'back to top' button

--- landing/src/components/Footer.tsx
+++ landing/src/components/Footer.tsx
@@ -3,6 +3,12 @@ import { Input } from './Input'
 const Footer: React.FC = () => {
   return (
     <footer className='w-full h-auto container p-2'>
+      <a
+        href='#'
+        className='hover:underline hover:text-gray-500 leading-4 cursor-pointer mt-6'
+      >
+        Back to top
+      </a>
       <div className='grid grid-cols-3 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 md:gap-8'>
         <div>
           <h2 className='font-bold text-2xl'>Timetracker</h2>
