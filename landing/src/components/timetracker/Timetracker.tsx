7e29cd0af09de3e529d358b8d416e71d732f47b5
barangana <baranganadriel@gmail.com>
2024-10-08 22:55:52 -0400

    feat(landing): added so users can change modes

--- landing/src/components/timetracker/Timetracker.tsx
+++ landing/src/components/timetracker/Timetracker.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Timer from './Timer'
 import Stopwatch from './Stopwatch'
 
 const Timetracker = () => {
-  //   const [activeComponent, setActiveComponent] = useState()
+  const [activeComponent, setActiveComponent] = useState<'Timer' | 'Stopwatch'>(
+    'Timer'
+  )
+
+  const showComponent = (component: 'Timer' | 'Stopwatch') => {
+    setActiveComponent(component)
+  }
+
   return (
     <div>
-      <Timer />
-      <Stopwatch />
+      {activeComponent === 'Timer' ? (
+        <div>
+          <Timer />
+          <button onClick={() => showComponent('Stopwatch')}>Stopwatch</button>
+        </div>
+      ) : (
+        <div>
+          <Stopwatch />
+          <button onClick={() => showComponent('Timer')}>Timer</button>
+        </div>
+      )}
     </div>
   )
 }
