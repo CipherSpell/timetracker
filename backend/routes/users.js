1b128a8f25d675995e5fdd60317f1014bf08a033
barangana <baranganadriel@gmail.com>
2024-11-24 16:09:46 -0500

    feat(backend): added some password validation on the backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,33 @@
-const router = require('express').Router();
-const { authenticateJWT } = require('../middlewares/auth.mw');
-const User = require('../models/User');
-const logger = require('../utilities/logger');
-const jwt = require('jsonwebtoken');
-const auth_utils = require('../utilities/auth.utils');
+const router = require("express").Router();
+const { authenticateJWT } = require("../middlewares/auth.mw");
+const User = require("../models/User");
+const logger = require("../utilities/logger");
+const jwt = require("jsonwebtoken");
+const auth_utils = require("../utilities/auth.utils");
 
-const JWT_SECRET = process.env.JWT_SECRET || 'PLACEHOLDER';
+const JWT_SECRET = process.env.JWT_SECRET || "PLACEHOLDER";
 
-router.get('/', authenticateJWT, async (req, res) => {
+router.get("/", authenticateJWT, async (req, res) => {
   let result = await User.getAllUsers();
   res.status(200).send(result);
 });
 
-router.post('/addUser', async (req, res) => {
-  const { email, password } = req.body; 
+router.post("/addUser", async (req, res) => {
+  const { email, password } = req.body;
+  const regexPassword =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*., ?])/;
 
   try {
     const existingUser = await User.getUserbyEmail(email);
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    if (!regexPassword.test(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters and contain a special character",
+      });
     }
 
     const hashedPassword = await auth_utils.hashPassword(password);
@@ -33,24 +42,24 @@ router.post('/addUser', async (req, res) => {
       email: newUser.email,
     };
 
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' });
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
 
     res.status(201).json({ token });
   } catch (error) {
     logger.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-router.get('/getUser/:email', authenticateJWT, async (req, res) => {
+router.get("/getUser/:email", authenticateJWT, async (req, res) => {
   const { email } = req.params;
 
   try {
     let result = await User.getUserbyEmail(email);
     res.status(200).send(result);
-  } catch(error) {
+  } catch (error) {
     logger.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
