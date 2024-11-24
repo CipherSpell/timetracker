0d5be77fbab9acf7b22040c981a840c1ea5522da
barangana <baranganadriel@gmail.com>
2024-11-24 15:52:12 -0500

    feat(landing): user is able to login

--- landing/src/components/auth/login/LoginActions.ts
+++ landing/src/components/auth/login/LoginActions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { userSchema } from '@/src/config/zod'
+import { redirect } from 'next/navigation'
 
 //TODO: modify "any" type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,4 +18,20 @@ export const getUser = async (prevState: any, formData: FormData) => {
   }
 
   // TODO: make request to backend
+  try {
+    const backendHost = process.env.BACKEND_HOST || 'http://backend:8080'
+    await fetch(`${backendHost}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      }),
+    })
+  } catch (error) {
+    console.error(error)
+  }
+  redirect('/')
 }
