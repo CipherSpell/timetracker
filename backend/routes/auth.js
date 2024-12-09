8c024199e8ca61545c294260298c2517c062e3e2
barangana <baranganadriel@gmail.com>
2024-12-08 21:10:37 -0500

    fix(backend): removed password validation on login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,19 +8,10 @@ const JWT_SECRET = process.env.JWT_SECRET || 'PLACEHOLDER';
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  const regexPassword =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*., ?])/;
 
   const user = await User.getUserbyEmail(email);
   if (!user || !(await auth_utils.validatePassword(password, user.password))) {
     return res.status(401).json({ message: 'Invalid credentials' });
-  }
-
-  if (!regexPassword.test(password)) {
-    return res.status(400).json({
-      message:
-        'Password must be at least 8 characters and contain a special character',
-    });
   }
 
   const payload = {
