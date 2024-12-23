f82336d813e7b197e29bbdfc1847c77ceb39b263
barangana <baranganadriel@gmail.com>
2024-12-22 19:46:50 -0500

    feat: created the cards for the report summary

---
+++ landing/src/components/ReportCards.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface ReportCardsProps {
+  time: number
+  text: string
+}
+
+const ReportCards = ({ time, text }: ReportCardsProps) => {
+  return (
+    <div className='mt-12'>
+      <div className='p-4 block max-w-48 bg-white border rounded-lg dark:bg-gray-900 dark:border-gray-900'>
+        <div className='mb-2 text-2xl font-bold dark:text-white'>{time}</div>
+        <p className='font-bold dark:text-white'>{text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ReportCards
